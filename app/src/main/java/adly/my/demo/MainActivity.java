0bf81605b8ed922668d24d5059c55dfd03819812
adlly <mm@ifnot.cc>
2016-11-01 20:59:48 +0800

    add storage size

--- app/src/main/java/adly/my/demo/MainActivity.java
+++ app/src/main/java/adly/my/demo/MainActivity.java
@@ -1,5 +1,6 @@
 package adly.my.demo;
 
+import android.content.SharedPreferences;
 import android.os.Environment;
 import android.os.StatFs;
 import android.support.v7.app.AppCompatActivity;
@@ -60,89 +61,113 @@ public class MainActivity extends AppCompatActivity {
 
     private void readView(){
 
-        FileInputStream fileInputStream = null;
-        try {
-            fileInputStream = new FileInputStream(file);
-            BufferedReader bufferedReader = new BufferedReader(new InputStreamReader(fileInputStream));
-//            String msg = bufferedReader.readLine();
-//            Log.e(this.getClass().getSimpleName(), msg != null ? msg : "msg为空");
-//            String[] s = msg.split("##");
+        SharedPreferences  sharedPreferences = getSharedPreferences("hh",MODE_PRIVATE);
+       String p1 = sharedPreferences.getString("name","");
+        String p2 = sharedPreferences.getString("psw","");
+        name.setText(p1);
+        psw.setText(p2);
+
+
+//        FileInputStream fileInputStream = null;
+//        try {
+//            fileInputStream = new FileInputStream(file);
+//            BufferedReader bufferedReader = new BufferedReader(new InputStreamReader(fileInputStream));
+////            String msg = bufferedReader.readLine();
+////            Log.e(this.getClass().getSimpleName(), msg != null ? msg : "msg为空");
+////            String[] s = msg.split("##");
+////
+////            if (s.length == 2) {
+////                name.setText(s[s.length-2]);
+////                psw.setText(s[s.length-1]);
+////            }else {
+////                Log.e(this.getClass().getSimpleName(), "数据不存在");
+////            }
+//            name.setText(bufferedReader.readLine());
+//            psw.setText(bufferedReader.readLine());
 //
-//            if (s.length == 2) {
-//                name.setText(s[s.length-2]);
-//                psw.setText(s[s.length-1]);
-//            }else {
-//                Log.e(this.getClass().getSimpleName(), "数据不存在");
-//            }
-            name.setText(bufferedReader.readLine());
-            psw.setText(bufferedReader.readLine());
-
-            fileInputStream.close();
-        } catch (FileNotFoundException e) {
-            e.printStackTrace();
-            Toast.makeText(this, "file not found", Toast.LENGTH_LONG).show();
-        } catch (IOException e) {
-            e.printStackTrace();
-        } finally {
-
-        }
+//            fileInputStream.close();
+//        } catch (FileNotFoundException e) {
+//            e.printStackTrace();
+//            Toast.makeText(this, "file not found", Toast.LENGTH_LONG).show();
+//        } catch (IOException e) {
+//            e.printStackTrace();
+//        } finally {
+//
+//        }
     }
 
     private void initView() {
 
-        btn.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View v) {
-                if (checkBox.isChecked()) {
-                    //写文件
 
-                    try {
-                        FileOutputStream fileOutputStream = new FileOutputStream(file);
-                        String s = name.getText().toString() + "\n" + psw.getText().toString();
-                        fileOutputStream.write(s.getBytes());
+    btn.setOnClickListener(new View.OnClickListener() {
+    @Override
+    public void onClick(View v) {
+        if(checkBox.isChecked()){
 
-                        fileOutputStream.close();
-                    } catch (FileNotFoundException e) {
-                        e.printStackTrace();
-                    } catch (IOException e) {
-                        e.printStackTrace();
-                    }
-
-                }
-                Toast.makeText(MainActivity.this, "登录成功！", Toast.LENGTH_LONG).show();
-            }
-        });
-
-        File file1 = Environment.getExternalStorageDirectory();
-        StatFs statFs = new StatFs(file1.getAbsolutePath());
-        long blockCountLong;
-        long blockSizeLong;
-        long availableBlocksLong;
-        long freeBlocksLong;
-        if (android.os.Build.VERSION.SDK_INT >= android.os.Build.VERSION_CODES.JELLY_BEAN_MR2) {
-            blockCountLong = statFs.getBlockCountLong();
-            blockSizeLong = statFs.getBlockSizeLong();
-            availableBlocksLong = statFs.getAvailableBlocksLong();
-            freeBlocksLong = statFs.getFreeBlocksLong();
-
-
-        }else{
-            blockCountLong = statFs.getBlockCount();
-            blockSizeLong = statFs.getBlockSize();
-            availableBlocksLong = statFs.getAvailableBlocks();
-            freeBlocksLong = statFs.getFreeBlocks();
-
+            SharedPreferences  sharedPreferences = getSharedPreferences("hh",MODE_PRIVATE);
+            SharedPreferences.Editor editor = sharedPreferences.edit();
+            editor.putString("name",name.getText().toString());
+            editor.putString("psw",psw.getText().toString());
+            editor.commit();
         }
+    }
+});
 
-        long l = blockSizeLong * availableBlocksLong;
-        String s = Formatter.formatFileSize(this, l);
 
-//        String s1 = Formatter.formatFileSize(this, blockSizeLong * blockCountLong);
 
-        text.setText(s + "\n" + Formatter.formatFileSize(this, blockSizeLong * blockCountLong)
-        + "\n" + Formatter.formatFileSize(this, blockSizeLong * (blockCountLong - freeBlocksLong))
-        + "\n" + Formatter.formatFileSize(this, freeBlocksLong * blockSizeLong));
-
+//        btn.setOnClickListener(new View.OnClickListener() {
+//            @Override
+//            public void onClick(View v) {
+//                if (checkBox.isChecked()) {
+//                    //写文件
+//
+//                    try {
+//                        FileOutputStream fileOutputStream = new FileOutputStream(file);
+//                        String s = name.getText().toString() + "\n" + psw.getText().toString();
+//                        fileOutputStream.write(s.getBytes());
+//
+//                        fileOutputStream.close();
+//                    } catch (FileNotFoundException e) {
+//                        e.printStackTrace();
+//                    } catch (IOException e) {
+//                        e.printStackTrace();
+//                    }
+//
+//                }
+//                Toast.makeText(MainActivity.this, "登录成功！", Toast.LENGTH_LONG).show();
+//            }
+//        });
+//
+//        File file1 = Environment.getExternalStorageDirectory();
+//        StatFs statFs = new StatFs(file1.getAbsolutePath());
+//        long blockCountLong;
+//        long blockSizeLong;
+//        long availableBlocksLong;
+//        long freeBlocksLong;
+//        if (android.os.Build.VERSION.SDK_INT >= android.os.Build.VERSION_CODES.JELLY_BEAN_MR2) {
+//            blockCountLong = statFs.getBlockCountLong();
+//            blockSizeLong = statFs.getBlockSizeLong();
+//            availableBlocksLong = statFs.getAvailableBlocksLong();
+//            freeBlocksLong = statFs.getFreeBlocksLong();
+//
+//
+//        }else{
+//            blockCountLong = statFs.getBlockCount();
+//            blockSizeLong = statFs.getBlockSize();
+//            availableBlocksLong = statFs.getAvailableBlocks();
+//            freeBlocksLong = statFs.getFreeBlocks();
+//
+//        }
+//
+//        long l = blockSizeLong * availableBlocksLong;
+//        String s = Formatter.formatFileSize(this, l);
+//
+////        String s1 = Formatter.formatFileSize(this, blockSizeLong * blockCountLong);
+//
+//        text.setText(s + "\n" + Formatter.formatFileSize(this, blockSizeLong * blockCountLong)
+//        + "\n" + Formatter.formatFileSize(this, blockSizeLong * (blockCountLong - freeBlocksLong))
+//        + "\n" + Formatter.formatFileSize(this, freeBlocksLong * blockSizeLong));
+//
 
     }
 
